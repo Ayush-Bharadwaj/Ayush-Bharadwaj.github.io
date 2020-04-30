e0143816105cf97a03163e3f561b4a254c0c7b37
ayush-bharadwaj <45692289+ayush-bharadwaj@users.noreply.github.com>
2020-04-30 15:28:27 +0530

    Added rps game

---
+++ sample/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ayush Bharadwaj(54821d18) - Broken Rock Paper Scissors</title>
+	</head>
+	<body>
+		<div class="container">
+
+		<h1>Welcome to Rock Paper Scissors</h1>
+		<p>
+			<a href="login.php">Please Log In</a>
+		</p>
+		<p>
+			Attempt to go to <a href="game.php">game.php</a> without logging in - it should fail with an error message.
+		</p>
+
+		</div>
+	</body>
+</html>
